fa448b5aba98a233522a5e1d55e6bc894de2793f
Tyler Wallace <tywallace2017@gmail.com>
2020-09-03 20:36:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,6 +10,7 @@ array_2 = [1, 1, 0
   ]
 
  find_min_in_nested_arrays = array_2
- find_min_in_nested_arrays
+ find_min_in_nested_array = array_1
 p find_min_in_nested_array
+p find_min_in_nested_arrays
 end
